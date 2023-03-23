537e98f85a296af0724a83350c1dabf2cd86940a
MyBooty165 <126267638+MyBooty165@users.noreply.github.com>
2023-03-23 16:09:21 -0500

    Create url_form.php

---
+++ templates/url_form.php
@@ -0,0 +1,26 @@
+
+<style type="text/css">
+
+html body {
+	margin-top: 50px !important;
+}
+
+#top_form {
+	position: fixed;
+	top:0;
+	left:0;
+	width: 100%;
+	
+	margin:0;
+	
+	z-index: 2100000000;
+	-moz-user-select: none; 
+	-khtml-user-select: none; 
+	-webkit-user-select: none; 
+	-o-user-select: none; 
+	
+	border-bottom:1px solid #151515;
+
+<script type="text/javascript">
+	smart_select(document.getElementsByName("url")[0]);
+</script>
